4f31850ae5d3d7d872dd6cc55bdc28f71c3e09c7
qwerty4uiop <alexpat930@gmail.com>
2017-10-07 20:32:14 +0300

    Fixed primex

--- primes.ex
+++ primes.ex
@@ -1,17 +1,17 @@
 defmodule Primes do
-    def isPrime(num) do
-        (2..num
-        |> Enum.filter(fn(x) -> rem(num, x) == 0 end)
-        |> length()) == 1
+    defp isPrime(num) do
+        (2..round(:math.sqrt(num))
+        |> Enum.filter(fn(x) -> rem(num, x) == 0 && num != 2 end)
+        |> length()) == 0
     end
     
-    def primes(n, _, counter) when counter == n + 1, do: []
+    defp primes(n, _, counter) when counter == n, do: []
 
-    def primes(n, acc, counter) do
+    defp primes(n, acc, counter) do
         if (isPrime(acc)), do: [acc | primes(n, acc + 1, counter + 1)], else: primes(n, acc + 1, counter)
     end
 
-    def printFirstPrimes(n), do: Enum.each(primes(n, 2, 1), fn(x) -> IO.puts(x) end)
+    def printFirstPrimes(n), do: Enum.each(primes(n, 2, 0), fn(x) -> IO.puts(x) end)
 end
 
 Primes.printFirstPrimes(1000)
